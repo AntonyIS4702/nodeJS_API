cb128d24ee0ec61a989d9d1b844f7174fafeaebd
AntonyIS4702 <antonyb0842@gmail.com>
2025-12-20 18:35:12 -0400

    db connection ok

--- login/src/modal/db.js
+++ login/src/modal/db.js
@@ -1,7 +1,7 @@
 // db.js
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://antonyb0842_db_user:A8101uca6368@cluster0.zz5n5qv.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://antonyb0842_db_user:A8101uca6368a@cluster0.zz5n5qv.mongodb.net/?retryWrites=true&w=majority";
 
 let client;
 let db;
@@ -10,10 +10,16 @@ async function connectDB() {
   if (db) return db; // reuse existing connection
 
   client = new MongoClient(uri);
-  await client.connect();
+  try {
+     await client.connect();
+       db = client.db("sample_mflix"); // change to your DB name
   console.log("✅ Connected to MongoDB Atlas");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+  }
+ 
 
-  db = client.db("sample_mflix"); // change to your DB name
+
   return db;
 }
 
