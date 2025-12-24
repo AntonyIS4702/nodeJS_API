b3463dafd97b57de8eff3b532d21481d202671e6
AntonyIS4702 <antonyb0842@gmail.com>
2025-12-23 20:53:21 -0400

    cors error fix

--- login/src/app.js
+++ login/src/app.js
@@ -8,7 +8,14 @@ const errorHandler = require('./middlewares/error.middleware');
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "http://localhost:3000","https://test-project-mocha-mu.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(morgan('dev'));
 
